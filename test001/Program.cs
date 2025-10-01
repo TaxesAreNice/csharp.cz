2bf9a170a793e7bc09ed9935b7128ba965542c80
Taxes AreNice <dataxtixer@gmail.com>
2025-10-01 10:11:21 +0200

    some stuff

--- test001/Program.cs
+++ test001/Program.cs
@@ -1,17 +1,40 @@
-﻿
+﻿bool a = true;
+int b = 5;
+int c = 4;
+bool d;
+int e = 5;
 
-int a = 5;
-int b = 3;
+bool vysledok = ((c <= b) || !a) && (a == (e == b));
 
-int sum = a + b;  // 8
-int difference = a - b;  // 2
-int product = a * b;   // 15
-double division = (double)a / b;  // 1.66666666666667
-int remainder = a % b;   // 2
+int s = 5;
 
-bool equality = a == b;  // false
-bool inequality = a != b;  // true
-bool greater = a > b;  // true
-bool lesser = a < b;  // false
-bool greaterEqual = a >= b;  // true
-bool lesserEqual = a <= b;  // false
+
+
+
+
+
+
+
+
+
+
+//using System.Reflection.Metadata;
+//using System.Security.AccessControl;
+
+//int a = 5;
+//int b = 3;
+
+//int sum = a + b;  // 8
+//int difference = a - b;  // 2
+//int product = a * b;   // 15
+//double division = (double)a / b;  // 1.66666666666667
+//int remainder = a % b;   // 2
+
+//bool equality = a == b;  // false
+//bool inequality = a != b;  // true
+//bool greater = a > b;  // true
+//bool lesser = a < b;  // false
+//bool greaterEqual = a >= b;  // true
+//bool lesserEqual = a <= b;  // false
+
+
