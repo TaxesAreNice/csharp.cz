d4d95a2eb91dcafc465bb3e9476adcebb2f1eeda
Taxes AreNice <dataxtixer@gmail.com>
2025-10-08 11:30:50 +0200

    loop! >;)

---
+++ kkk/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+int u = 1;
+//change one thing and its a infinity loop :>
+while (1 <= 1000)
+{
+    Console.WriteLine(u);
+    u++;
+}
+*/
+
+//loop
+for (int i = 1; i <= 1000; i++)
+{
+    //to make it go like 2,4,6,8...
+    //int modulo = i % 2;
+    //if (modulo == 0) 
+        //the write thing ofc
+    {
+        Console.WriteLine(i);
+    }
+}
