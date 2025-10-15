9e1971ceb3dd17e6f13def3f69e2260afdb42010
Taxes AreNice <dataxtixer@gmail.com>
2025-10-15 12:54:08 +0200

    example for funcions

---
+++ grutings/Program.cs
@@ -0,0 +1,29 @@
+﻿string name = GetMyName();
+string age = GetMyAge();
+
+PrintStuff(name, age);
+
+
+
+
+string GetMyName()
+{
+    Console.WriteLine("Name?");
+    string? name = Console.ReadLine();
+
+    return name;
+}
+
+string GetMyAge()
+{
+    Console.WriteLine("Age?");
+    string? age = Console.ReadLine();
+
+    return age;
+}
+
+
+void PrintStuff(string name, string age)
+{
+    Console.WriteLine($"Your name is {name} and you're {age} years old");
+}
