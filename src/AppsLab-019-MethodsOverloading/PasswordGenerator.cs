dc6579341bd6841e2be75f9ab0f77fc163d340e0
Taxes AreNice <dataxtixer@gmail.com>
2025-11-05 13:06:51 +0100

    the password generator

--- src/AppsLab-019-MethodsOverloading/PasswordGenerator.cs
+++ src/AppsLab-019-MethodsOverloading/PasswordGenerator.cs
@@ -18,7 +18,8 @@ namespace AppsLab_019_MethodsOverloading
         /// <returns>A randomly generated password.</returns>
         public string GeneratePassword()
         {
-            throw new NotImplementedException();
+            
+          return GeneratePassword(DefaultLength);
         }
 
         /// <summary>
@@ -28,7 +29,15 @@ namespace AppsLab_019_MethodsOverloading
         /// <returns>A randomly generated password.</returns>
         public string GeneratePassword(int length)
         {
-            throw new NotImplementedException();
+            string password = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                    int randomNumber = _random.Next(27);
+                    password = password + Alphabet[randomNumber].ToString();
+            }
+
+            return password;
         }
 
         /// <summary>
